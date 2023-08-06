0e6a4ff34bbfd23ec8d7ab147e03618f71fc2667
Varlotte <32116877+Varlotte@users.noreply.github.com>
2023-08-06 18:45:04 -0400

    passes one test

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+  "what does the this keyword mean?",
+  "What is the Constructor OO pattern?",
+  "implementing Blockchain Web API",
+  "The Test Driven Development Workflow",
+  "What is NaN and how Can we Check for it",
+  "What is the difference between stopPropagation and preventDefault?",
+  "Immutable State and Pure Functions",
+  "what is the difference between == and ===?",
+  "what is the difference between event capturing and bubbling?",
+  "what is JSONP?",
 ];
 
-const titleCased = () => {
-  return tutorials
+function wordToTitleCase(word) {
+  return word.charAt(0).toUpperCase() + word.substr(1);
 }
+
+const titleCased = () => {
+  return tutorials.map((tutorial) =>
+    tutorial.split(" ").map(wordToTitleCase).join(" ")
+  );
+};
+
+//.map through each word in each string in this array
+//string.startsWith(string).toUpperCase
